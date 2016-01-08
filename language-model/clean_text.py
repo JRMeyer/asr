efe38cfdc1f73a4baebea37f19f402c5f3541d48
JRMeyer <joshua.richard.meyer@gmail.com>
2016-01-08 16:42:36 -0700

    added a new dir for all scripts dealing with the language model, and split ngrams.py into two scripts. Now ngrams.py expects a text file that has been cleaned (only Kyrgyz letters occur, no punctuation, each line is a sentence). All pre-processing happens in clean_text.py

---
+++ language-model/clean_text.py
@@ -0,0 +1,61 @@
+import re
+
+def clean_lines_in_file(fileName,kyrgyzLetters):
+    '''
+    Given a path to a text file
+    (1) split file into lines where sentences should be (see regex object)
+    (2) tokenize each line (see function tokenize_line()
+    (3) add each line as a character string to 'lines'
+    '''
+    regex = re.compile(r'[.!?\n]')
+    outFile = open('cleaned_text.txt', 'w')
+    with open(fileName) as inFile:
+        content = inFile.read()
+        for line in re.split(regex,content):
+            line = tokenize_line(line,kyrgyzLetters)
+            if line != '':
+                print(line, file=outFile)
+            else:
+                pass
+    outFile.close()
+
+
+def tokenize_line(line,kyrgyzLetters):
+    '''
+    (1) lower all text, strip whitespace and newlines
+    (2) replace everything that isn't a letter or space
+    (3) split line on whitespace
+    (4) pad the line
+    (5) return tokens
+    '''
+    line = line.lower().strip().rstrip()
+    # regex pattern to match everything that isn't a letter
+    pattern = re.compile('[\W_0-9]+', re.UNICODE)
+    # replace everything that isn't a letter or space
+    line = (' ').join([pattern.sub('', token) for token in line.split(' ')])
+    tokens=[]
+    for token in line.split(' '):
+        if token == '':
+            pass
+        # make sure we only have Kyrgyz letters in token
+        elif all(char in kyrgyzLetters for char in token):
+            tokens.append(token)
+    # check if there are tokens, then pad the line
+    if tokens:
+        line = (' ').join(tokens)
+        line = '<s> ' + line + ' </s>'
+    else:
+        line = ''
+    return line
+
+kyrgyzLetters = ['а','о','у','ы','и','е','э',
+                'ө','ү','ю','я','ё','п','б',
+                'д','т','к','г','х','ш','щ',
+                'ж','з','с','ц','ч','й','л',
+                'м','н','ң','ф','в','р','ъ',
+                'ь']
+
+if __name__ == "__main__":
+    import sys
+    infile = sys.argv[1]
+    clean_lines_in_file(infile, kyrgyzLetters)
