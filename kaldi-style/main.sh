b74725be76b868bb4e74444c10d6dbc060aa3fb4
JRMeyer <joshua.richard.meyer@gmail.com>
2017-01-30 01:17:28 +0100

    no real changes

--- kaldi-style/main.sh
+++ kaldi-style/main.sh
@@ -22,17 +22,17 @@ silence_phone="SIL"
 unknown_word="<unk>"
 unknown_phone="SPOKEN_NOISE"
 
-ngramOrder=1
+ngramOrder=3
 ngramFile="task.arpabo"
 
 
-# # delete old versions of the files this script makes
-# # using -f just to supress error message if the files dont already exist
-# rm -f $clean_corpus $phoneticDict $phoneticDict_NOSIL $phonesList \
-#     $ngramFile 
+# delete old versions of the files this script makes
+# using -f just to supress error message if the files dont already exist
+rm -f $clean_corpus $phoneticDict $phoneticDict_NOSIL $phonesList \
+    $ngramFile 
 
-# # first clean the corpus
-# ./clean_text.pl $messy_corpus $minSentenceLength $addSilence > $clean_corpus
+# first clean the corpus
+./clean_text.pl $messy_corpus $minSentenceLength $addSilence > $clean_corpus
 
 # make the phonetic dictionary and list of phones
 ./make_dict.pl \
@@ -44,11 +44,11 @@ ngramFile="task.arpabo"
     --silence_phone $silence_phone \
     --unknown_word $unknown_word \
     --unknown_phone $unknown_phone \
-    --stress;
-    #--graphemes; \
+    --graphemes;
+    #--stress;
 
-# # make the ngram language model
-# ./make_lm.sh $clean_corpus $ngramFile $ngramOrder
+# make the ngram language model
+./make_lm.sh $clean_corpus $ngramFile $ngramOrder
 
 # sort files by bytes (kaldi-style) and re-save them with orginal filename
 for fileName in $phoneticDict $phoneticDict_NOSIL $phonesList; do 
