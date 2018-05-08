d352788fee20993bc7d80a3e6b06daf2cf178e96
Josh Meyer <joshuameyer@email.arizona.edu>
2018-05-08 06:56:15 -0500

    cleaner script

--- kaldi-style/converting-nnet3-egs-to-csv/egs-to-csv.py
+++ kaldi-style/converting-nnet3-egs-to-csv/egs-to-csv.py
@@ -1,11 +1,6 @@
 import sys
 import re
 
-infile=sys.argv[1]
-regex = re.compile("dim=96 \[ ([0-9]*) ")
-
-
-
 
 
 def extract_windows(eg, line, outfile, win_size=29):
@@ -14,34 +9,56 @@ def extract_windows(eg, line, outfile, win_size=29):
     this function will extract windows of a given size and assign them
     to their label in a label -- flattened_data file
     '''
-    
+
+    # for each label in the nnet3Egs object
     for i, label in enumerate(regex.findall(line)):
-        
-        rowVector=''
+
+        # cat all feats for that eg/label into a single vector
+        catFeats=''
         for row in eg[i:i+win_size]:
-            rowVector += (row[0] + ' ')
-
-        print(label, rowVector, file=outfile)
-
-
+            catFeats += (row[0] + ' ')
+            
+        print(label, catFeats, file=outfile)
 
 
-eg=[]
-with open(infile,"r") as f:
-    with open("output.txt","a") as outfile:
-        for line in f:
-            # the first line of the eg
-            if 'input' in line:
-                eg=[]
-                pass
-            # if we've hit the labels then we're at the end of the data
-            elif 'output' in line:
-                extract_windows(eg, line, outfile)
-                # this should be one frame of data
-            else:
-                eg.append([line.strip()])
+
+
+
+
+def main(arkfile, regex, outfile):
+
+    eg=[]
+    with open(arkfile,"r") as arkf:
+        with open(outfile,"a") as outf:
+            for line in arkf:
+                # the first line of the eg
+                if 'input' in line:
+                    eg=[]
+                    pass
+                # if we've hit the labels then we're at the end of the data
+                elif 'output' in line:
+                    extract_windows(eg, line, outf)
+                    # this should be one frame of data
+                else:
+                    eg.append([line.strip()])
                 
 
 
+if __name__ == "__main__":
+        
+    # this is a kaldi nnet3Egs ark file which has been converted
+    # to txt via nnet3-copy-egs
+    arkfile=sys.argv[1] 
 
+    # this regex matches the label for
+    # each eg in a frame of egs (ie the
+    # ark file contains groups of egs
+    # which make sense for TDNN)
+    regex = re.compile("dim=96 \[ ([0-9]*) ") 
+
+    outfile="output.txt"
+    
+    main(arkfile, regex, outfile)
+
+    print("Extracted egs from " + arkfile + " and printed to " + outfile )
     
